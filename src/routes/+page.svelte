462d978cb2e2308f0ad95918fd10a771b5ed943c
chris <actuallychrismendez@gmail.com>
2024-05-01 16:24:09 -0700

    added header & footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<div class="hero min-h-screen bg-calm">
-  <div class="hero content text-center">
-    <h2></h2>
-    <div class='mockup-code text-start backdrop-blur-md'>
+
+<div data-theme='dracula' class="hero h-fit w-full">
+  <div data-theme='' class="hero content text-center p-2">
+    <div class='mockup-code text-start backdrop-blur-md shadow-lg'>
       <pre data-prefix="$"><code>ls <span class='text-green-300'>chris</span></code></pre>
       <pre data-prefix=">"><code>-----------</code></pre>
       <pre data-prefix=">"><code class='text-warning'>./BS/<span class='text-error'>computer_science</span></code></pre>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
+  dasiyui: {
+    themes: [
+      "dark",
+      "light",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+    ],
+  },
   theme: {
     extend: {
       backbroundImage: (theme) => ({
